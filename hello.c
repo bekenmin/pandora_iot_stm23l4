761f25027ced77d5bbd20e9f0685e649a7aea181
wangmin <wangmin@wangmindeMacBook-Air.local>
2023-10-27 19:51:58 +0800

    add scanf input

--- hello.c
+++ hello.c
@@ -1,5 +1,9 @@
 #include "stdio.h"
 int main (){
     printf("hello world!\r\n");
+    int a, b;
+    printf("please enter a and b :\r\n");
+    scanf("%d, %d", &a, &b);
+    printf("a = %d, b =%d, a + b =%d \r\n", a, b, a +b);
     return 0;
 }
